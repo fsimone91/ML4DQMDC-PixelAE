a590916416495a313481b5847379c9e146141c50
fsimone <federica.maria.simone@cern.ch>
2023-08-11 16:57:49 +0200

    minor modifications 2022C data

---
+++ dqmioprod/crab/rucio.sh
@@ -0,0 +1,54 @@
+#rucio add-rule \
+#cms:/Muon1/Run2023C-v1/RAW#3eeffcab-8401-4d4c-a7ef-117bc9632ba2 1 \
+#T2_IT_Bari \
+#--lifetime 2592000 \
+#--activity "User AutoApprove" \
+#--ask-approval
+
+
+rucio add-rule \
+cms:/Muon1/Run2023C-v1/RAW#50813b06-e8e7-47ad-bfef-693173ca4b0c 1 \
+T2_IT_Bari \
+--lifetime 2592000 \
+--activity "User AutoApprove" \
+--ask-approval
+
+
+rucio add-rule \
+cms:/Muon1/Run2023C-v1/RAW#ed4d4a84-fcc6-43b0-ab28-aa048a344dc7 1 \
+T2_IT_Bari \
+--lifetime 2592000 \
+--activity "User AutoApprove" \
+--ask-approval
+
+
+rucio add-rule \
+cms:/Muon1/Run2023C-v1/RAW#18d53df6-34bb-4506-88c5-8c62a6a11a5e 1 \
+T2_IT_Bari \
+--lifetime 2592000 \
+--activity "User AutoApprove" \
+--ask-approval
+
+
+rucio add-rule \
+cms:/Muon1/Run2023C-v1/RAW#2abe0f31-f04d-4920-8883-db3c4a9a258b 1 \
+T2_IT_Bari \
+--lifetime 2592000 \
+--activity "User AutoApprove" \
+--ask-approval
+
+
+rucio add-rule \
+cms:/Muon1/Run2023C-v1/RAW#84e4c183-9958-4c6b-9223-d1614283fe97 1 \
+T2_IT_Bari \
+--lifetime 2592000 \
+--activity "User AutoApprove" \
+--ask-approval
+
+
+rucio add-rule \
+cms:/Muon1/Run2023C-v1/RAW#8bedb6e9-e42c-4e12-827e-379610049c14 1 \
+T2_IT_Bari \
+--lifetime 2592000 \
+--activity "User AutoApprove" \
+--ask-approval
